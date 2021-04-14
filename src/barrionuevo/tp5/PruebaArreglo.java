98146b1c70c9b2faf5cef8698348afce62d4caca
Pablo <Pablo@NOTEBOOK-PABLO>
2021-04-14 14:33:44 -0300

    Comentario del Ejercicio

--- src/barrionuevo/tp5/PruebaArreglo.java
+++ src/barrionuevo/tp5/PruebaArreglo.java
@@ -1,5 +1,26 @@
 package barrionuevo.tp5;
-
+/*****************************************************************************
+**Trabajo práctico 5: Arreglos en Java.
+**Alumno: Barrionuevo Pablo.
+**Materia: Laboratorio 1.
+**Consigna: 
+* En un nuevo proyecto, crear una clase de nombre Arreglo con los siguientes métodos
+* estáticos (static):
+* a) Método sumarLista que reciba por parámetro un arreglo unidimensional de
+* enteros y muestre por pantalla la suma y promedio de los mismos.
+* b) Método buscarMayor que reciba por parámetro un arreglo bidimensional e
+* irregular de enteros y retorne el entero más grande que se encuentra en el arreglo.
+* c) Método cuentaVocales, que reciba por parámetro un String y retorne la cantidad
+* de vocales que tiene la cadena.
+* d) Método cuentaCaracter que reciba por parámetro un String y un caracter, luego
+* retorne la cantidad de veces que se repite en la cadena el carácter recibido.
+* Luego desde el método main de una clase de nombre PruebaArreglo, invocar los métodos
+* de la clase Arreglo creada por usted.
+* Subir el proyecto a un repositorio GIT y enviar el link del mismo a través de la tarea.
+**
+* Repositorio GIT:
+* https://github.com/PabloBarrionuevo/Arreglos-Java/tree/master
+*****************************************************************************/
 public class PruebaArreglo {
     public static void main(String[] args) {
         int[] pruebasumarLista = {1,2,3,4,5,6,7,8,9,0}; // 45, 45/10
